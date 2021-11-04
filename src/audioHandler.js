40222f0c350c86f4439ed4bfb2605c872e3659ad
Phineas Ziegler <phinziegler@gmail.com>
2021-11-04 12:03:37 -0400

    added small delay to multi-piece move sounds

--- src/audioHandler.js
+++ src/audioHandler.js
@@ -1,32 +1,26 @@
 const moveSounds = [
-    "./sounds/pieceMove1.wav",
     "./sounds/pieceMove2.wav",
     "./sounds/pieceMove3.wav",
     "./sounds/pieceMove4.wav",
     "./sounds/pieceMove5.wav",
 ];
 export default class AudioHandler {
-    constructor() {
-    }
-
-
+    // RETURN RANDOM SOUND FROM LIST
     static randomSound() {
         const sound = moveSounds[Math.floor(Math.random()*(moveSounds.length))];
         return sound;
-
     }
 
-    static playSound(audio) {
-        audio.play();
-    }
-
-    static moveSound(multi) {
-        let audio1 = new Audio(moveSounds[0]);
-        audio1.play();
+    // PLAY MOVE SOUND. IF MANY PIECES MOVED, PLAY A DELAYED SOUND AS WELL
+    static moveSound(multi, delay=50) {
+        const audio1 = new Audio("./sounds/pieceMove1.wav");
+        const audio2 = new Audio(AudioHandler.randomSound());
+        const delay = 50;
         if(multi) {
-            let audio2 = new Audio(AudioHandler.randomSound());
-            console.log("multiple pieces moved.");
-            audio2.play();
+            setTimeout(() => {
+                audio2.play();
+            }, delay);
         }
+        audio1.play();
     }
 }
